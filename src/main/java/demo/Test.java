8c269c65a0a31b9f30389e00436910e8f66f4a1d
Praveen <Praveen@Praveen>
2022-04-19 12:37:23 +0530

    First Commit

---
+++ src/main/java/demo/Test.java
@@ -0,0 +1,21 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.google.co.in/");
+		
+		Thread.sleep(3000);
+		driver.close();		
+
+	}
+
+}
